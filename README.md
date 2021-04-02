c79baa00596227840fc07938a8c0f7db8f54e139
Justin Pattison-Schmidt <purdueschmidt@users.noreply.github.com>
2021-04-01 23:05:31 -0400

    Add Hw5a code and test

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # 567GitHubApi
 
+##HW04a
 [![Build Status](https://travis-ci.com/purdueschmidt/567GitHubApi.svg?branch=main)](https://travis-ci.com/purdueschmidt/567GitHubApi)
+
+##HW05a
+[![Build Status](https://travis-ci.com/purdueschmidt/567GitHubApi.svg?branch=hw05a_mocking)](https://travis-ci.com/purdueschmidt/567GitHubApi)
 
--- test_github_api.py
+++ test_github_api.py
@@ -1,70 +1,122 @@
 from unittest import main, TestCase
+from unittest.mock import Mock, patch,MagicMock,PropertyMock
 from  github_api import RepoData
 import  github_api
 from github import Github
 
 import unittest
 
-class stubRepo :
-    def __init__(self,name,commits):
-        self.name = name
-        self.commits =commits
-        
+#from github import Github
+
+
+class mock_repo:
+    name = 'test'
+    def get_repos(self,user):
+        return []  
     def get_commits(self):
-        return self.commits
+        return []
     
-class stubApi:
-
-    def __init__(self,repos):
-        self.repos = repos
-        
     def get_user(self,username):
-        return self
+        pass
     
-    def get_repos(self):
-        return self.repos
-
-
 class TestGithubApi(TestCase):
+
+    @patch('github_api.Github')
+    def test_no_repo(self, mock_Github):
+        repodata = RepoData()
+ 
+
+        mock_Github.get_user.return_value.get_repos.return_value = []
+
+
+        ret = repodata.getRepoInfo(mock_Github, 'test')
+
+        mock_Github.get_user.assert_called_once_with('test')
+        mock_Github.get_user.return_value.get_repos.assert_called_once()
+        mock_Github.get_commits.assert_not_called()
+
+        assert ret == ''
     
-    def test_rediculus(self):
+    @patch('github_api.Github')
+    def test_single_repo_one_commit(self, mock_Github):
         repodata = RepoData()
-        gitApi = Github()
+ 
+        inst = Mock(spec=mock_repo)
+        mock_Github.get_user.return_value.get_repos.return_value = [inst]
 
-        ret = repodata.getRepoInfo(gitApi, 'purdueschmidt')
-        text=''
-        for repo in gitApi.get_user('purdueschmidt').get_repos() :
-            numCommits = len(list(repo.get_commits()))
-            text += 'Repo: ' + repo.name + ' Number of commits: ' + str(numCommits) + '\n'
-            
-        assert ret == text
-
-    def test_one_repo_multi_commits(self):
-        repodata = RepoData()
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.return_value = [2]
         
-        gitRepo = [stubRepo('repoa',[1,1])]
-        gitApi = stubApi(gitRepo)
+        p=PropertyMock(return_value='Mocked_Name')
+        type(inst).name=p
 
-        ret = repodata.getRepoInfo(gitApi, 'purdueschmidt')
-        assert ret == 'Repo: repoa Number of commits: 2\n'
-
-    def test_one_repo_one_commits(self):
-        repodata = RepoData()
+        ret = repodata.getRepoInfo(mock_Github, 'test')
         
-        gitRepo = [stubRepo('repoa',[1])]
-        gitApi = stubApi(gitRepo)
+        mock_Github.get_user.assert_called_once_with('test')
+        mock_Github.get_user.return_value.get_repos.assert_called_once()
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.assert_called_once()
+        assert ret == 'Repo: Mocked_Name Number of commits: 1\n'
 
-        ret = repodata.getRepoInfo(gitApi, 'purdueschmidt')
-        assert ret == 'Repo: repoa Number of commits: 1\n'
-
-    def test_multi_repo_one_commit(self):
+    @patch('github_api.Github')
+    def test_single_repo_no_commit(self, mock_Github):
         repodata = RepoData()
+ 
+        inst = Mock(spec=mock_repo)
+        mock_Github.get_user.return_value.get_repos.return_value = [inst]
+
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.return_value = []
+
+        p=PropertyMock(return_value='Mocked_Name')
+        type(inst).name=p
+
+        ret = repodata.getRepoInfo(mock_Github, 'test')
         
-        gitRepo = [stubRepo('repoa',[1]),stubRepo('repob',[1])]
-        gitApi = stubApi(gitRepo)
+        mock_Github.get_user.assert_called_once_with('test')
+        mock_Github.get_user.return_value.get_repos.assert_called_once()
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.assert_called_once()
+        assert ret == 'Repo: Mocked_Name Number of commits: 0\n'
+        
+    @patch('github_api.Github')
+    def test_single_repo_multi_commit(self, mock_Github):
+        repodata = RepoData()
+ 
+        inst = Mock(spec=mock_repo)
+        mock_Github.get_user.return_value.get_repos.return_value = [inst]
 
-        ret = repodata.getRepoInfo(gitApi, 'purdueschmidt')
-        assert ret == 'Repo: repoa Number of commits: 1\nRepo: repob Number of commits: 1\n'
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.return_value = [1,1,1,1,1]
 
+        p=PropertyMock(return_value='Mocked_Name')
+        type(inst).name=p
+
+        ret = repodata.getRepoInfo(mock_Github, 'test')
+        
+        mock_Github.get_user.assert_called_once_with('test')
+        mock_Github.get_user.return_value.get_repos.assert_called_once()
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.assert_called_once()
+        assert ret == 'Repo: Mocked_Name Number of commits: 5\n'
+        
+    @patch('github_api.Github')
+    def test_multi_repo_commit_one_none(self, mock_Github):
+        repodata = RepoData()
+ 
+        inst = Mock(spec=mock_repo)
+        inst2 = Mock(spec=mock_repo)
+        mock_Github.get_user.return_value.get_repos.return_value = [inst,inst2]
+
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.return_value = [1]
+        mock_Github.get_user.return_value.get_repos.return_value[1].get_commits.return_value = []
+
+        a=PropertyMock(return_value='Mocked_Name')
+        type(inst).name=a
+        b=PropertyMock(return_value='Mocked_Name2')
+        type(inst2).name=b
+
+        ret = repodata.getRepoInfo(mock_Github, 'test')
+
+        mock_Github.get_user.assert_called_once_with('test')
+        mock_Github.get_user.return_value.get_repos.assert_called_once()
+        mock_Github.get_user.return_value.get_repos.return_value[0].get_commits.assert_called_once()
+        mock_Github.get_user.return_value.get_repos.return_value[1].get_commits.assert_called_once()
+        assert ret == 'Repo: Mocked_Name Number of commits: 1\nRepo: Mocked_Name2 Number of commits: 0\n'
+        
 if __name__ == '__main__':
     main()
